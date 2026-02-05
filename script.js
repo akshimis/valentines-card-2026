f24c4a4a6d510477e29b699ba215051bab0911c1
akshi <mistr049@umn.edu>
2026-02-05 16:06:26 +0530

    fix bugs, add letter content

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const envelope = document.getElementById('envelope');
+  const letterContainer = document.getElementById('letter');
 
-    envelope.addEventListener('click', () => {
-    envelope.classList.toggle('open');
-    });
+  envelope.addEventListener('click', () => {
+    envelope.classList.add('open');
+
+    // automatically show the letter after the envelope opens
+    setTimeout(() => {
+      letterContainer.classList.add('show');
+    }, 1000); // 1 second delay
+  });
 });
